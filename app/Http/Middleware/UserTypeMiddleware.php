f7aad4de9eb0236ddd7d890b7b0f8c7026a7693f
Gautam Jivan <jivangautam@neputer.com>
2024-03-25 16:40:04 +0545

    fix: fixed auth

--- app/Http/Middleware/UserTypeMiddleware.php
+++ app/Http/Middleware/UserTypeMiddleware.php
@@ -30,8 +30,10 @@ class UserTypeMiddleware
                 }
                 return redirect()->route('user.dashboard');
             }
+        }else{
+            return redirect()->route('login');
         }
-        return $next($request);
+
     }
 
 }
