4d9dc744a5bafbfc3badda1c492e0d63d11df0b9
Gautam Jivan <jivangautam@neputer.com>
2024-03-25 18:02:45 +0545

    chore: WIP testAdminUserCanDeleteNormalUserData

--- tests/Feature/admin/AdminDashboardControllerTest.php
+++ tests/Feature/admin/AdminDashboardControllerTest.php
@@ -2,21 +2,43 @@
 
 namespace Tests\Feature\admin;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AdminDashboardControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testAdminCanAccessDashboard()
+    use RefreshDatabase;
+
+    public function testUnauthorizedUserCannotAccessAdminDashboard(): void
     {
         $response = $this->get(route('admin.dashboard'));
+        $response->assertRedirect(route('login'));
+    }
 
+    public function testAuthorizedUserCanAccessAdminDashboard(): void
+    {
+        $role = 1;
+        $user = $this->createAdminUser($role);
+        $response = $this->actingAs($user)->get(route('admin.dashboard'));
         $response->assertStatus(200);
     }
+
+    public function testAdminUserCanDeleteNormalUserData()
+    {
+        $role = 0;
+        $user = $this->createAdminUser($role);
+        $id = $user['id'];
+        $response = $this->delete(route('admin.user.delete', ['id' => $id]));
+        $response->assertRedirect(route('admin.user'));
+        $this->assertDatabaseMissing('users', ['id' => $id]);
+        $response->assertSessionHas('alert-success');
+    }
+    protected function createAdminUser($role): Model
+    {
+        return User::factory()->createOne(['role'=>$role]);
+    }
+
 }
