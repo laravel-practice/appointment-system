fb886c9ac0d6e69c6f14e7668b22976e2efea145
Gautam Jivan <jivangautam@neputer.com>
2024-03-25 18:03:54 +0545

    chore: WIP PHPUnitTest for delete user

--- tests/Feature/admin/AdminDashboardControllerTest.php
+++ tests/Feature/admin/AdminDashboardControllerTest.php
@@ -26,16 +26,16 @@ class AdminDashboardControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function testAdminUserCanDeleteNormalUserData()
-    {
-        $role = 0;
-        $user = $this->createAdminUser($role);
-        $id = $user['id'];
-        $response = $this->delete(route('admin.user.delete', ['id' => $id]));
-        $response->assertRedirect(route('admin.user'));
-        $this->assertDatabaseMissing('users', ['id' => $id]);
-        $response->assertSessionHas('alert-success');
-    }
+//    public function testAdminUserCanDeleteNormalUserData()
+//    {
+//        $role = 0;
+//        $user = $this->createAdminUser($role);
+//        $id = $user['id'];
+//        $response = $this->delete(route('admin.user.delete', ['id' => $id]));
+//        $response->assertRedirect(route('admin.user'));
+//        $this->assertDatabaseMissing('users', ['id' => $id]);
+//        $response->assertSessionHas('alert-success');
+//    }
     protected function createAdminUser($role): Model
     {
         return User::factory()->createOne(['role'=>$role]);
