901f3572c8d0f87c6a927c5e6ba91e351bd95480
Jivan Gautam <jivangautam@neputer.com>
2024-05-07 18:45:53 +0545

    fix: additional testing for admin dashboard

--- tests/Feature/admin/AdminDashboardControllerTest.php
+++ tests/Feature/admin/AdminDashboardControllerTest.php
@@ -5,38 +5,48 @@ namespace Tests\Feature\admin;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AdminDashboardControllerTest extends TestCase
 {
     use RefreshDatabase;
-
+    
     public function testUnauthorizedUserCannotAccessAdminDashboard(): void
     {
         $response = $this->get(route('admin.dashboard'));
         $response->assertRedirect(route('login'));
+
     }
 
     public function testAuthorizedUserCanAccessAdminDashboard(): void
     {
         $role = 1;
-        $user = $this->createAdminUser($role);
+        $user = $this->createUser($role);
         $response = $this->actingAs($user)->get(route('admin.dashboard'));
         $response->assertStatus(200);
     }
 
-//    public function testAdminUserCanDeleteNormalUserData()
-//    {
-//        $role = 0;
-//        $user = $this->createAdminUser($role);
-//        $id = $user['id'];
-//        $response = $this->delete(route('admin.user.delete', ['id' => $id]));
-//        $response->assertRedirect(route('admin.user'));
-//        $this->assertDatabaseMissing('users', ['id' => $id]);
-//        $response->assertSessionHas('alert-success');
-//    }
-    protected function createAdminUser($role): Model
+    public function testAdminUserCanDeleteNonAdminUserData()
+    {
+        // create admin user
+        $adminUser = $this->createUser(1);
+
+        // create non-admin user
+        $nonAdminUser = $this->createUser(0);
+
+        // Create history to support 'redirect()->back()' in actual controller
+        $this->actingAs($adminUser)
+            ->get(route('admin.user'));
+
+        $this->actingAs($adminUser)
+            ->delete(route('admin.user.delete', ['id' => $nonAdminUser['id']]))
+            ->assertRedirect(route('admin.user'))
+            ->assertSessionHas('alert-success');
+
+        $this->assertDatabaseMissing('users', ['id' => $nonAdminUser['id']]);
+    }
+
+    protected function createUser($role): Model
     {
         return User::factory()->createOne(['role'=>$role]);
     }
